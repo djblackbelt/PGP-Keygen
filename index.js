1b0d050baaccb44d946b17a1a082a0f44b1505ef
Derek Larkins <dlarkins.gm@gmail.com>
2020-02-26 04:07:15 -0700

    working ez

---
+++ index.js
@@ -0,0 +1,37 @@
+const utils = require('./utils.js');
+const inquirer = require('inquirer');
+const openpgp = require('openpgp');
+
+async function init() {
+    inquirer.prompt(utils.menu).then(async function(ans) {
+        try {
+            if(ans.pass1 !== ans.pass2){
+                console.log('ERROR: Passwords do not match!');
+                process.exit(1);
+            }
+            let keypair = await generateKeypair(ans.userId, ans.userEmail, ans.pass1);
+            console.log(`Public Key Armored: \n${keypair.pub}`);
+            console.log(`Private Key Armored: \n${keypair.priv}`);
+            console.log(`Revoc. Certificate: \n${keypair.rev}`);
+        } catch (error) {
+            console.log('ERROR: init() ' + error);
+            process.exit(1);
+        }
+    });
+}
+
+async function generateKeypair(uid, uem, pp) {
+    const { privateKeyArmored, publicKeyArmored, revocationCertificate } = await openpgp.generateKey({
+        userIds: [{ name: uid, email: uem}],
+        curve: 'ed25519',
+        passphrase: pp
+    });
+    let ret = {
+        'pub': publicKeyArmored,
+        'priv': privateKeyArmored,
+        'rev': revocationCertificate
+    };
+    return ret;
+}
+
+init();
---
+++ utils.js
@@ -0,0 +1,26 @@
+var menu = [
+    {
+        type: "input",
+        name: "userId",
+        message: "Please enter your user ID: "
+    },
+    {
+        type: "input",
+        name: "userEmail",
+        message: "Please enter your email: "
+    },
+    {
+        type: "password",
+        name: "pass1",
+        message: "Please enter a password\n"
+    },
+    {
+        type: "password",
+        name: "pass2",
+        message: "Please confirm your password\n"
+    }
+];
+
+module.exports = {
+    menu
+};
